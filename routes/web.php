2fdbc8f023c47de893d597517c8f464cf9069a96
joy210 <md.sohanur.rahman.joy@gmail.com>
2019-12-22 01:50:45 +0600

    update

--- routes/web.php
+++ routes/web.php
@@ -13,4 +13,21 @@
 
 Route::get('/', function () {
     return view('welcome');
+})->name('home');
+
+Auth::routes();
+
+Route::get('/home', 'HomeController@index');
+
+
+Route::group(['as' => 'admin.', 'prefix'=>'admin','namespace' => 'Admin','middleware' => ['auth', 'admin']], function (){
+
+    Route::get('dashboard', 'DashboardController@index')->name('dashboard');
+
+});
+
+Route::group(['as' => 'author.', 'prefix'=>'author', 'namespace'=> 'Author', 'middleware' => ['auth', 'author']], function (){
+
+    Route::get('dashboard', 'DashboardController@index')->name('dashboard');
+
 });
